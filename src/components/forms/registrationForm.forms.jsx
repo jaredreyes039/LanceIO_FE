41cf13b8e27058fcbae77a0ccbf4f61d1a639e22
Jay Reyes <jaredreyes039@gmail.com>
2026-01-10 14:11:13 -0700

    Registration form now implemented with react-hook-form and validated with yup as a separate component

--- src/components/forms/registrationForm.forms.jsx
+++ src/components/forms/registrationForm.forms.jsx
@@ -1,106 +1,80 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TextInput from '../ui/inputs/Input.component.jsx'
 import { ToastContainer, toast } from 'react-toastify';
-import { validateFormData } from "../../utils/validateFormData.util"
+import { useForm, SubmitHandler, FormProvider } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { registerSchema } from "../../utils/formValidationSchema.util"
 
 export default function RegistrationForm() {
 
-	const [username, setUsername] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [confirmPassword, setConfirmPassword] = useState("");
-	const [tosAgreed, setTosAgreed] = useState(false);
-	const [subscribed, setSubscribed] = useState(false);
+	// TODO: ADD PASSWORD REQUIREMENT CHECKLIST
 
-	const handleChangeUsername = (e) => {
-		setUsername(e.target.value);
-	}
-	const handleChangeEmail = (e) => {
-		setEmail(e.target.value);
-	}
-	const handleChangePassword = (e) => {
-		setPassword(e.target.value);
-	}
-	const handleChangeConfirmPassword = (e) => {
-		setConfirmPassword(e.target.value);
-	}
-	const handleSubscription = (e) => {
-		setSubscribed((prev) => !prev);
+	const {
+		register,
+		reset,
+		handleSubmit,
+		watch,
+		formState: { errors }
+	} = useForm({ resolver: yupResolver(registerSchema) });
+
+	const methods = { register, reset }
+
+	function onSubmit(data) {
+		// TODO: INTERFACE WITH USERS SERVER
+		console.log(data);
 	}
 
-	const getCompiledFormData = () => {
-		const formData = new FormData();
-		formData.append("username", username);
-		formData.append("password", password);
-		formData.append("email", email);
-		return formData;
-	}
-
-	const handleSubmission = (e) => {
-		// TODO: ADD FORM SANITIZATION FUNCTION HERE
-		e.preventDefault();
-		validateFormData(getCompiledFormData());
-		if (confirmPassword !== password) {
-			toast.error("Passwords do not match, please check your entires and try again.");
-			return;
-		}
-
-		if (process.env.NODE_ENV === "development") {
-			toast.success("Succesfully registered, check localStorage to analyze payload.")
-		}
-		if (process.env.NODE_ENV === "production") {
-			// TODO: RECONNECT WITH BACKEND USER SERVICE WHEN LANCEIO_USERS SERVER IS READY FOR DEPLOYMENT
-		}
-		return;
-	}
+	useEffect(() => {
+		reset({
+			name: "data",
+		})
+	}, [reset])
 
 	return (
 		<>
 			<ToastContainer />
-			<form onSubmit={(e) => { handleSubmission(e) }}>
-				<TextInput
-					type="text"
-					label="Username"
-					inputName="username"
-					direction="column"
-					placeholder="Enter username"
-					changeHandler={handleChangeUsername}
-					required={true}
-				/>
-				<TextInput
-					type="email"
-					label="Email Address"
-					inputName="email"
-					direction="column"
-					placeholder="Enter email address"
-					changeHandler={handleChangeEmail}
-					required={true}
-				/>
-				<TextInput
-					type="password"
-					label="Password"
-					inputName="password"
-					direction="column"
-					placeholder="Enter password"
-					changeHandler={handleChangePassword}
-					required={true}
-				/>
-				<TextInput
-					type="password"
-					label="Confirm Password"
-					inputName="confirmPassword"
-					direction="column"
-					placeholder="Confirm password"
-					changeHandler={handleChangeConfirmPassword}
-					required={true}
-				/>
-				{/* TODO: INSERT TOS AGREEMENT AND SUB OPTION HERE */}
-				<button
-					class="btn btn-submit"
-					type="submit">
-					Register
-				</button>
-			</form >
+			<FormProvider {...methods}>
+				<span className="form-error">{errors.username?.message}</span>
+				<span className="form-error">{errors.email?.message}</span>
+				<span className="form-error">{errors.password?.message}</span>
+				<span className="form-error">{errors.confirmPassword?.message}</span>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<TextInput
+						type="text"
+						label="Username"
+						inputName="username"
+						direction="column"
+						placeholder="Enter username"
+					/>
+					<TextInput
+						type="email"
+						label="Email Address"
+						inputName="email"
+						direction="column"
+						placeholder="Enter email address"
+					/>
+					<TextInput
+						type="password"
+						label="Password"
+						inputName="password"
+						direction="column"
+						placeholder="Enter password"
+					/>
+					<TextInput
+						type="password"
+						label="Confirm Password"
+						inputName="confirmPassword"
+						direction="column"
+						placeholder="Confirm password"
+					/>
+					{/* TODO: INSERT TOS AGREEMENT AND SUB OPTION HERE */}
+					<button
+						class="btn btn-submit"
+						type="submit">
+						Register
+					</button>
+				</form >
+			</FormProvider>
 		</>
 	);
 }
